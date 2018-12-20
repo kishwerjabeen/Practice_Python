499da1701ebeff5258b690875832580bf65677cc
kishwerjabeen <kishwerjabeen90@gamil.com>
2018-12-20 11:36:49 -0800

    Pratice_Python

--- Escape_python.py
+++ Escape_python.py
@@ -1,3 +1,15 @@
 print("this is \\\\ double backslash") #print_double_back_slash
-print('this is a /\//\/\ mountin')
+print("this is a /\\/\\/\\ mountin")
 print("he is \t awsum")
+
+# shortcut or R 
+
+print( r"line A /  line B")
+
+# print emojee
+# https://unicode.org/emoji/charts/full-emoji-list.html
+
+print(" \U0001F600 ")
+
+print ("\U0001F618")
+
