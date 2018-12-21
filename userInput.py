a8a165e4cf39dd39eca6a056532365b7ca68edba
kishwerjabeen <kishwerjabeen90@gmail.com>
2018-12-21 09:05:33 -0800

    Practice_Python

---
+++ userInput.py
@@ -0,0 +1,11 @@
+# user input
+# input function
+name = input("Type your name ")
+print("hello " + name)
+# string 
+age = input("what is your age ? ")
+#24 , "24"
+print("your age is " + age)
+number_one = int(input("enter first number : "))
+number_two = int(input("enter second number : "))
+print("total is " number_one + number_two) 
