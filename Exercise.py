1c4cf54c02723363ecde98dcc12b99a00726fdac
kishwerjabeen <kishwerjabeen90@gmail.com>
2018-12-21 13:09:05 -0800

    string_python

---
+++ Exercise.py
@@ -0,0 +1,14 @@
+num1 = input("Enter 1st number : ")
+num2 = input("Enter 2nd number : ")
+num3 = input("Enter 3rd number : ")
+add = int(num1) + int(num2) + int(num3)
+ans=add/3
+print(ans)
+
+#or another format 
+ans= (int(num1) + int(num2) + int(num3))/3
+print("avrage of three number " + str(  ans))
+
+
+#or 
+
